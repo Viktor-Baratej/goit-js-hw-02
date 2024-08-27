5e984350c2a497db256287604f60649306ec4578
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2024-08-27 18:21:31 +0300

    add js file

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,8 @@
 function formatMessage(message, maxLength) {
-    if (message.Length <= maxLength) {
-        return message;
-    } else {
-        return message.slice(0, maxLength) + "...";
-    }
-    
-}
+    return message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
+}  
+// Цей варіант використовує тернарний оператор для більш лаконічного запису логіки перевірки та обрізання рядка.
+
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
@@ -13,8 +10,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-
-
-
-
